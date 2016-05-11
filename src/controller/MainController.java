20aff9776d4c9263061facf45145043e7de346d1
Artur Nowicki <arturnowicki@gmail.com>
2016-05-11 15:44:48 +0200

    clear and backspace
    added backspace and clear button
    still don’t work properly

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -9,10 +9,10 @@ import model.Calculations;
 public class MainController {
 
 	@FXML
-	private TextField answerField;
+	private TextField currentCalculation;	
 	@FXML
-	private Button btnEquals;
-	
+	private TextField answerField;
+
 	private String operator = "";
 	private String firstNumber = "";
 	private boolean isStart = true;
@@ -22,8 +22,36 @@ public class MainController {
 		Object source = event.getSource();
 		if(source instanceof Button) {
 			if(!isStart) answerField.clear();
+			currentCalculation.appendText(((Button) source).getText());
 			answerField.appendText(((Button) source).getText());
 			isStart = true;
+		}
+	}
+	
+	public void processControl(ActionEvent event) {
+		Object source = event.getSource();
+		if(source instanceof Button) {
+			String value = ((Button) source).getText();
+			if(value.equals("C")) {
+				answerField.clear();
+				isStart = true;
+				operator = "";
+				firstNumber = "";
+			}
+			if(value.equals("←")) {
+				System.out.println(answerField.getText().length());
+				if(answerField.getText().length()==1) {
+					System.out.println("aa1");
+					answerField.clear();
+					System.out.println("aa2");
+					currentCalculation.deleteText(answerField.getText().length()-1, answerField.getText().length());
+					System.out.println("aa3");
+					return;
+				}
+				if(answerField.getText().length()==0) return;
+				answerField.deleteText(answerField.getText().length()-1, answerField.getText().length());
+				currentCalculation.deleteText(answerField.getText().length()-1, answerField.getText().length());
+			}
 		}
 	}
 
@@ -33,10 +61,14 @@ public class MainController {
 			String value = ((Button) source).getText();
 			if(!"=".equals(value)) {
 				operator = value;
+				if(!isStart) currentCalculation.appendText(answerField.getText());
+				currentCalculation.appendText(value);
 				firstNumber = answerField.getText();
 				answerField.clear();
 			} else {
+				if(firstNumber.equals("")) return;
 				answerField.setText(calc.calculate(firstNumber, operator, answerField.getText()));
+				currentCalculation.appendText(" = " + answerField.getText() + ";");
 				isStart=false;
 			}
 		}
